e3179286a10457c01c50b39ee08536a40f548afa
Scott Reilly <scott@coffe2code.com>
2009-04-12 18:55:33 -0400

    Release v1.0:
    * Remove 'parents' argument and stuff related to it
    * Tweak formatting
    * Add extended description
    * Add readme.txt
    * Updated copyright date

--- peer-categories.php
+++ peer-categories.php
@@ -1,28 +1,72 @@
 <?php
 /*
 Plugin Name: Peer Categories
-Version: 0.6.1
-Plugin URI: http://www.coffee2code.com/wp-plugins/peer-categories
+Version: 1.0
+Plugin URI: http://coffee2code.com/wp-plugins/peer-categories
 Author: Scott Reilly
-Author URI: http://www.coffee2code.com
-Description: Display only the peer categories for a given post's categories (but not parent categories!)
+Author URI: http://coffee2code.com
+Description: Display only the categories that are peer (i.e. share the same category parent) to all lowest-level assigned categories for the specified post.
 
-Compatible with WordPress 2.5+ and 2.6+.
+	This plugin provides a template tag which acts a modified version of WordPress's built-in template tag, `the_category()`.  
+	`the_category()` lists all categories directly assigned to the specified post.  `peer_categories()` lists those categories
+	*PLUS* any categories that are peer to those categories and *MINUS* categories that are parents to other assigned
+	categories.  Peer categories are categories that all share the same category parent.
+
+	For example, assume your category structure is hierarchical and looks like this:
+
+	Vegetables
+	|-- Leafy
+	|   |-- Broccoli
+	|   |-- Bok Choy
+	|   |-- Celery
+	|-- Fruiting
+	|   |-- Bell Pepper
+	|   |-- Cucumber
+	|   |-- Pumpkin
+	|-- Podded
+	|   |-- Chickpea
+	|   |-- Lentil
+	|   |-- Soybean
+
+	If you directly assigned the categories "Fruiting" and "Pumpkin" to a post, `peer_categories()` would return a list that consists
+	of: "Bell Pepper", "Cucumber", and "Pumpkin".  Notice that since "Fruiting" was a parent to a directly assigned category, its peers
+	are not included in the list.  If only "Fruiting" were selected as a category, then "Leafy", "Fruiting", and "Podded" would have been
+	listed.
+
+	By default, categories are listed as an HTML list.  The first argment to the template tag allows you to define a custom separator,
+	e.g. to have a simple comma-separated list of peer categories: `<?php peer_categories(','); ?>`.
+
+	As with categories listed via `the_category()`, categories that are listed are presented as links to the respective category's
+	archive page.
+
+	Examples:
+
+	`<?php peer_categories(); ?>`
+	Displays something like:
+	    `<ul><li><a href="http://yourblog.com/category/books">Books</a></li>
+	    <li><a href="http://yourblog.com/category/movies">Movies</a></li></ul>`
+
+	`<?php peer_categories(','); ?></ul>`
+	Displays something like:
+	    `<a href="http://yourblog.com/category/books">Books</a>, <a href="http://yourblog.com/category/movies">Movies</a>`
+
+Compatible with WordPress 2.5+, 2.6+, 2.7+.
 
 =>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
 =>> for more information and the latest updates
 
 Installation:
 
-1. Download the file http://www.coffee2code.com/wp-plugins/peer-categories.zip and unzip it into your 
+1. Download the file http://coffee2code.com/wp-plugins/peer-categories.zip and unzip it into your 
 wp-content/plugins/ directory.
-2. (optional) Add filters for 'peer_category' to filter peer category listing
-3. Use the function peer_category() somewhere inside "the loop"
+2. Activate the plugin through the 'Plugins' admin menu in WordPress
+3. (optional) Add filters for 'peer_categories' to filter peer category listing
+4. Use the template tag <?php peer_categories() ?> somewhere inside "the loop"
 
 */
 
 /*
-Copyright (c) 2008 by Scott Reilly (aka coffee2code)
+Copyright (c) 2008-2009 by Scott Reilly (aka coffee2code)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
 files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
@@ -38,17 +82,17 @@ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 /*
-	When in-the-loop, 
+	In the loop 
 */
-function peer_category($separator = '', $parents='', $post_id = false) {
-	echo get_peer_category_list($separator, $parents, $post_id);
+function peer_categories( $separator = '', $post_id = false ) {
+	echo get_peer_categories_list($separator, $post_id);
 }
 
-function get_peer_category_list($separator = '', $parents='', $post_id = false) {
+function get_peer_categories_list( $separator = '', $post_id = false ) {
 	global $wp_rewrite;
-	$categories = get_peer_category($post_id);
-	if (empty($categories))
-		return apply_filters('peer_category', __('Uncategorized'), $separator, $parents);
+	$categories = get_peer_categories($post_id);
+	if ( empty($categories) )
+		return apply_filters('peer_categories', __('Uncategorized'), $separator);
 
 	$rel = ( is_object($wp_rewrite) && $wp_rewrite->using_permalinks() ) ? 'rel="category tag"' : 'rel="category"';
 
@@ -57,22 +101,8 @@ function get_peer_category_list($separator = '', $parents='', $post_id = false) 
 		$thelist .= '<ul class="post-categories">';
 		foreach ( $categories as $category ) {
 			$thelist .= "\n\t<li>";
-			switch ( strtolower($parents) ) {
-				case 'multiple':
-					if ($category->parent)
-						$thelist .= get_category_parents($category->parent, TRUE);
-					$thelist .= '<a href="' . get_category_link($category->term_id) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' . $rel . '>' . $category->name.'</a></li>';
-					break;
-				case 'single':
-					$thelist .= '<a href="' . get_category_link($category->term_id) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' . $rel . '>';
-					if ($category->parent)
-						$thelist .= get_category_parents($category->parent, FALSE);
-					$thelist .= $category->name.'</a></li>';
-					break;
-				case '':
-				default:
-					$thelist .= '<a href="' . get_category_link($category->term_id) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' . $rel . '>' . $category->cat_name.'</a></li>';
-			}
+			$thelist .= '<a href="' . get_category_link($category->term_id) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' .
+						$rel . '>' . $category->cat_name.'</a></li>';
 		}
 		$thelist .= '</ul>';
 	} else {
@@ -80,31 +110,16 @@ function get_peer_category_list($separator = '', $parents='', $post_id = false) 
 		foreach ( $categories as $category ) {
 			if ( 0 < $i )
 				$thelist .= $separator . ' ';
-			switch ( strtolower($parents) ) {
-				case 'multiple':
-					if ( $category->parent )
-						$thelist .= get_category_parents($category->parent, TRUE);
-					$thelist .= '<a href="' . get_category_link($category->term_id) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' . $rel . '>' . $category->cat_name.'</a>';
-					break;
-				case 'single':
-					$thelist .= '<a href="' . get_category_link($category->term_id) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' . $rel . '>';
-					if ( $category->parent )
-						$thelist .= get_category_parents($category->parent, FALSE);
-					$thelist .= "$category->cat_name</a>";
-					break;
-				case '':
-				default:
-					$thelist .= '<a href="' . get_category_link($category->term_id) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' . $rel . '>' . $category->name.'</a>';
-			}
+			$thelist .= '<a href="' . get_category_link($category->term_id) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' . $rel . '>' . $category->name.'</a>';
 			++$i;
 		}
 	}
-	return apply_filters('peer_category', $thelist, $separator, $parents);
+	return apply_filters('peer_categories', $thelist, $separator);
 }
 
-function get_peer_category($id = false) {
+function get_peer_categories( $id = false ) {
 	$categories = get_the_category($id);
-	if (empty($categories)) {
+	if ( empty($categories) ) {
 		return get_categories(array('hide_empty' => false, 'user_desc_for_title' => false, 'title_li' => '', 'child_of' => 0, 'depth' => 1));
 	}
 
@@ -112,15 +127,15 @@ function get_peer_category($id = false) {
 	$parents = array();
 
 	// Go through all categories and get, then filter out, parents.
-	foreach ($categories as $c) { $parents[] = $c->parent; }
-	foreach ($categories as $c) {
+	foreach ( $categories as $c ) { $parents[] = $c->parent; }
+	foreach ( $categories as $c ) {
 		if ( !in_array($c->term_id, $parents) ) { $peers[] = $c; }
 	}
 	
 	// For each cat at this point, get peer cats.
 	$parents = array();
-	foreach ($peers as $c) {
-		if ($c->parent) {
+	foreach ( $peers as $c ) {
+		if ( $c->parent ) {
 			$parents[] = $c->parent;
 		} else {
 			$parents[] = 0;
@@ -128,14 +143,14 @@ function get_peer_category($id = false) {
 	}
 	$parents = array_unique($parents);
 	$peers = array();
-	foreach ($parents as $p) {
+	foreach ( $parents as $p ) {
 		$args = array('hide_empty' => false, 'user_desc_for_title' => false, 'title_li' => '', 'child_of' => $p, 'depth' => 0);
 		$cats = get_categories($args);
-		# If this cat has no parent, then we only want root categories
-		if ($p == 0 ) {
+		# If this cat has no parent, then only get root categories
+		if ( $p == 0 ) {
 			$new_peers = array();
 			foreach ($cats as $c) {
-				//TODO? We might also want to add extra conditional clause of !in_array($c->parent, $parents)
+				//TODO? Might also want to add extra conditional clause of !in_array($c->parent, $parents)
 				if ($c->parent == 0) $new_peers[] = $c;
 			}
 		} else {
@@ -143,14 +158,8 @@ function get_peer_category($id = false) {
 		}
 		$peers = array_merge($peers, $new_peers);
 	}
-	usort($peers,'peer_sort');
+	usort($peers, '_usort_terms_by_name');
 	return $peers;
 }
-function peer_sort($a,$b) {
-	$al = strtolower($a->cat_name);
-	$bl = strtolower($b->cat_name);
-	if ($al == $bl)
-	    return 0;
-	return ($al > $bl) ? +1 : -1;
-}
+
 ?>
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== Peer Categories ===
+Contributors: Scott Reilly
+Donate link: http://coffee2code.com
+Tags: categories, category, peer, sibling, related posts, similar posts, list, the_category
+Requires at least: 2.5
+Tested up to: 2.7.1
+Stable tag: trunk
+Version: 1.0
+
+Display only the categories that are peer (i.e. share the same category parent) to all lowest-level assigned categories for the specified post.
+
+== Description ==
+
+Display only the categories that are peer (i.e. share the same category parent) to all lowest-level assigned categories for the specified post.
+
+This plugin provides a template tag which acts a modified version of WordPress's built-in template tag, `the_category()`.  `the_category()` lists all categories directly assigned to the specified post.  `peer_categories()` lists those categories *PLUS* any categories that are peer to those categories and *MINUS* categories that are parents to other assigned categories.  Peer categories are categories that all share the same category parent.
+
+For example, assume your category structure is hierarchical and looks like this:
+
+Vegetables
+|-- Leafy
+|   |-- Broccoli
+|   |-- Bok Choy
+|   |-- Celery
+|-- Fruiting
+|   |-- Bell Pepper
+|   |-- Cucumber
+|   |-- Pumpkin
+|-- Podded
+|   |-- Chickpea
+|   |-- Lentil
+|   |-- Soybean
+
+If you directly assigned the categories "Fruiting" and "Pumpkin" to a post, `peer_categories()` would return a list that consists of: "Bell Pepper", "Cucumber", and "Pumpkin".  Notice that since "Fruiting" was a parent to a directly assigned category, its peers are not included in the list.  If only "Fruiting" were selected as a category, then "Leafy", "Fruiting", and "Podded" would have been listed.
+
+By default, categories are listed as an HTML list.  The first argment to the template tag allows you to define a custom separator, e.g. to have a simple comma-separated list of peer categories: `<?php peer_categories(','); ?>`.
+
+As with categories listed via `the_category()`, categories that are listed are presented as links to the respective category's archive page.
+
+Examples:
+
+`<?php peer_categories(); ?>`
+Displays something like:
+    `<ul><li><a href="http://yourblog.com/category/books">Books</a></li>
+    <li><a href="http://yourblog.com/category/movies">Movies</a></li></ul>`
+
+`<?php peer_categories(','); ?></ul>`
+Displays something like:
+    `<a href="http://yourblog.com/category/books">Books</a>, <a href="http://yourblog.com/category/movies">Movies</a>`
+
+
+== Installation ==
+
+1. Unzip `peer-categories-v1.0.zip` inside the `/wp-content/plugins/` directory for your site
+1. Activate the plugin through the 'Plugins' admin menu in WordPress
+1. (optional) Add filters for 'peer_categories' to filter peer category listing
+1. Use the template tag `<?php peer_categories(); ?>` somewhere inside "the loop"
+
+== Frequently Asked Questions ==
+
+= Why isn't an assigned category for the post showing up in the 'peer_categories()' listing? =
+
+If an assigned category is the parent for one or more other assigned categories for the post, then the category parent is not included in the listing.  Only peers to the lowest-level assigned categories are considered.
+
+
