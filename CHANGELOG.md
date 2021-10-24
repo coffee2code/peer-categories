7482c9d602e2f96b8a04aec0442da474433bdcea
Scott Reilly <scott@coffee2code.com>
2021-10-23 23:43:22 -0700

    Unit tests: In bootstrap, store path to plugin file constant

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@
         * Change: Move `phpunit/` into `tests/phpunit/`
         * Change: Move `phpunit/bin/` into `tests/`
     * Change: Remove 'test-' prefix from unit test file
+    * Change: In bootstrap, store path to plugin file constant
 
 ## 2.1.5 _(2021-04-17)_
 * Change: Note compatibility through WP 5.7+
--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -5,6 +5,8 @@
  * @package Peer_Categories
  */
 
+define( 'PEER_CATEGORIES_PLUGIN_FILE', dirname( __FILE__, 3 ) . '/peer-categories.php' );
+
 ini_set( 'display_errors', 'on' );
 error_reporting( E_ALL );
 
@@ -20,7 +22,7 @@ require_once $_tests_dir . '/tests/phpunit/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( __FILE__, 3 ) . '/peer-categories.php';
+	require PEER_CATEGORIES_PLUGIN_FILE;
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
